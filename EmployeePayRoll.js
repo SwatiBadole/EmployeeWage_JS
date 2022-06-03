497c20be84d1ca818ef6ad250a0a723dc160cd6e
SwatiBadole <swatibadole1997@gmail.com>
2022-06-03 13:31:36 -0700

    Added:Use Regex Pattern - Use Try Catch in case of Error

--- EmployeePayRoll.js
+++ EmployeePayRoll.js
@@ -17,28 +17,36 @@ class EmployeePayrollData
 		this.gender=params[3];
 		this.startDate=params[4];
 	}
-
-        get name()
+    get name()
     {
         return this._name;
     }
     set name(name)
     {
-        this._name=name;
+        let nameRegex=RegExp('^[A-Z]{1}[a-z]{3,}$');
+        if (nameRegex.test(name))
+            this._name=name;
+        else
+            throw 'Name is Incorrect!';
     }
     toString()
     {
         const options={year:'numeric',month:'long',day:'numeric'};
         const empDate=this.startDate===undefined ? "undefined": this.startDate.toLocaleDateString("en-US",options);
-
         return "id="+this.id+",name="+this.name+"salary="+this.salary+", "+"gender="+this.gender+",startDate="+empDate;
     }
-
 }
-
-let employeePayrollData=new EmployeePayrollData(1,"Mark",30000);
-console.log(employeePayrollData.toString());
-employeePayrollData.name="john";
-console.log(employeePayrollData.toString());
-let newEmployeePayrollData = new EmployeePayrollData(1,"Terrisa",30000,"F",new Date());
-console.log(newEmployeePayrollData.toString());
+    let employeePayrollData= new EmployeePayrollData(1,"Mark",30000);
+    console.log(employeePayrollData.toString());
+    try
+    {
+        employeePayrollData.name="jhon";
+        console.log(employeePayrollData.toString());
+    }
+    catch(e)
+    {
+        console.error(e);
+    }
+    let newEmployeePayrollData= new EmployeePayroll
+    Data(1,"Terrisa",30000,"F",new Date());
+    console.log(newEmployeePayrollData.toString());
